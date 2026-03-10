9067258a7e21f768e21b708c6aa4561381b146a4
MUNEZERO Alpha <alphamnzr@gmail.com>
2026-03-10 09:51:39 +0200

    db

--- server/server.js
+++ server/server.js
@@ -2,18 +2,22 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
+import { connectDB } from "./lib/db.js";
 
 //
 const app = express();
 const server = http.createServer(app)
 
 //middleware setup
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
-app.use(express.json({limit: "4mb"}));
 
-app.use("api/status", (req, res) => {
-    res.status(200).json({message: "Server is running"});
+app.use("/api/status", (req, res) => {
+    res.send("Server is running");
 });
+
+
+await connectDB();
 
 const PORT = process.env.PORT || 5000;
 
